dfad7ff870b7545b7c9756d28b78e2ae5fc8dac3
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-03-30 16:22:56 -0500

    Add ability to filter by rating

--- frontend/src/components/StoriesPage.jsx
+++ frontend/src/components/StoriesPage.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 import Story from '../components/Story';
+import StarRatings from './StarRatings';
 
 function StoriesPage() {
     useEffect( () => {
@@ -11,6 +12,7 @@ function StoriesPage() {
     const [stories, setStories] = useState([]);
     const [genres, setGenres] = useState([]);
     const [genreFilters, setGenreFilters] = useState([]);
+    const [ratingFilter, setRatingFilter] = useState([]);
     const [dataLoaded, setDataLoaded] = useState(false);
 
     const fetchItems = async () => {
@@ -37,6 +39,24 @@ function StoriesPage() {
         }
     }
 
+    const handleRatingFilterChange = (e) => {
+        const target_rating = Number.parseInt(e.target.value);
+        setRatingFilter([target_rating])
+    }
+    
+    const renderRatingFilter = (rating, showAndUp) => {
+        return (
+            <label key={rating}>
+                <input type="radio" name="ratings" value={rating} />
+                <StarRatings
+                    disabled
+                    rating={rating}
+                />
+                {showAndUp && <span>& up</span>}
+            </label>
+        )
+    }
+
     if (!dataLoaded) {
     return <div>Loading stories...</div>
     }
@@ -48,15 +68,34 @@ function StoriesPage() {
             <section className="section section--genre-filters">
                 <div className="container-fluid">
                     <form onChange={handleGenreFilterChange}>
-                        <fieldset className="genres-fieldset">
-                            <details className="genres-dropdown">
+                        <fieldset className="filters-fieldset">
+                            <details className="filters-dropdown">
                                 <summary>Genres</summary>
-                                <div className="genres">
+                                <div className="filters">
                                     {genres.map(({genre_name}) => (
                                     <label key={genre_name}>
                                         <input type="checkbox" name="genre_names" value={genre_name} />
                                         <span>{genre_name}</span>
                                     </label>
+                                    ))}
+                                </div>
+                            </details>
+                            
+                        </fieldset>
+                    </form>
+
+                    <form onChange={handleRatingFilterChange}>
+                        <fieldset className="filters-fieldset">
+                            <details className="filters-dropdown">
+                                <summary>Ratings</summary>
+                                <div className="filters">
+                                    <label key={0}>
+                                        <input type="radio" name="ratings" value={0} />
+                                        <span>All</span>
+                                    </label>
+                                    {renderRatingFilter(5)}
+                                    {[4, 3, 2, 1].map((genre_name) => (
+                                        renderRatingFilter(genre_name, true)
                                     ))}
                                 </div>
                             </details>
@@ -69,9 +108,17 @@ function StoriesPage() {
             <section className="section section--stories">
                 {
                     (
-                        genreFilters.length === 0 ?
-                        stories :
-                        stories.filter(story => story.genre_names && genreFilters.some(genre_name => story.genre_names.includes(genre_name)))
+                        (
+                        stories.filter(story => {
+                            return (
+                                genreFilters.length === 0
+                                || story.genre_names && genreFilters.some(genre_name => story.genre_names.includes(genre_name))
+                            ) && (
+                                ratingFilter.length === 0
+                                || story.avg_rating && story.avg_rating >= ratingFilter[0]
+                            )
+                        })
+                        )
                     )
                     .map(item => (
                         <Link className="stories-page__story-link-wrapper" to={`/stories/${item.story_id}`}>
